d3e00b329eb7457923ab047aaadbb655dafc621f
decker.233 <decker.233@server3.ies.cse.ohio-state.edu>
2014-11-14 01:04:37 -0500

    Added ORM classes started recreating h2 class for database

--- BudgetCatWebSite/src/H2/H2.java
+++ BudgetCatWebSite/src/H2/H2.java
@@ -5,24 +5,49 @@ public class H2 {
 
 	public static void createTables(){
 		try{
+			Statement stmt = null;
 			Connection conn = null;
-			conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/BudgetID", "sa", "");
+			
+			Class.forName("org.h2.Driver");
+			
+			conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/BudgetID", "decker233", "sammy");
+			
+			stmt = conn.createStatement();
+			stmt.execute("");
+			stmt.execute("");
+			stmt.execute("");
+			stmt.execute("");
+			stmt.execute("");
+			stmt.execute("");
+			stmt.execute("");
 			
 			conn.close();
 		}catch(Exception e)
 		{
-			System.out.println("There was an issue creatingTables" + e.getMessage());
+			System.out.println("There was an issue creatingTables: " + e);
 		}
 	}
+	
+	
 	public static void deleteTables(){
 		try{
+			Statement stmt = null;
 			Connection conn = null;
-			conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/BudgetID", "sa", "");
+			
+			Class.forName("org.h2.Driver");
+			
+			conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/BudgetID", "decker233", "sammy");
+			
+			String sql = "DROP TABLE IF EXISTS User, Account, Account_Type, Budget, Transaction, Category, User_Categories";
+			
+			stmt = conn.createStatement();
+			
+			stmt.execute(sql);
 			
 			conn.close();
 		}catch(Exception e)
 		{
-			System.out.println("There was an issue deletingTables" + e.getMessage());
+			System.out.println("There was an issue deletingTables: " + e);
 		}
 	}
 }
--- BudgetCatWebSite/src/Part3Demo/Part3Demo.java
+++ BudgetCatWebSite/src/Part3Demo/Part3Demo.java
@@ -1,10 +1,16 @@
 package Part3Demo;
-
+import H2.H2;
 import ValidationAndParsing.validateXSD;
 
 public class Part3Demo {
 	public static void main(String[] args){
-	validateXSD.validateXMLAgainstSchema("XML/Budgets.xml");
+	// clear tables for empty h2 database for demonstration
+    H2.deleteTables();
 	
+	// Add tables to the database if they don't already exist
+	H2.createTables();
+	
+	//validate xml against schema before parsing and adding data to database
+	validateXSD.validateXMLAgainstSchema("XML/Budgets.xml");
 	}
 }
